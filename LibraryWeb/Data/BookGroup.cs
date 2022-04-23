4defee07ab57a80014deccf67530e50bbcb74928
Виктор Яковлев <yava.victor.yava@gmail.com>
2022-04-23 14:58:20 +0300

    Частичный перевод by BBad

--- LibraryWeb/Data/BookGroup.cs
+++ LibraryWeb/Data/BookGroup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,20 +9,34 @@ namespace LibraryWeb.Models
 {
     public enum BookGroup
     {
+        [Display(Name = "Акустика")]
         Acoustics,
+        [Display(Name = "Физика")]
         Physics,
+        [Display(Name = "Математика")]
         Mathematics,
+        [Display(Name = "Астрономия")]
         Astronomy,
+        [Display(Name = "Биология")]
         Biology,
+        [Display(Name = "Информатика")]
         Informatics,
+        [Display(Name = "Инжинерия")]
         Engineering,
+        [Display(Name = "Строительная инжинерия")]
         BuildingEngineering,
+        [Display(Name = "История")]
         History,
         NonFiction,
+        [Display(Name = "Архитектура")]
         Architecture,
+        [Display(Name = "Дизайн")]
         Design,
+        [Display(Name = "Графический дизайн")]
         GraphicDesign,
+        [Display(Name = "Индустриальный дизайн")]
         IndustrialDesign,
+        [Display(Name = "Искусство")]
         Art,
 
     }
